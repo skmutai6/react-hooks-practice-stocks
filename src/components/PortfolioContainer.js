dcd11b447bfb2f813444f9d6de6a2ff437a77946
skmutai6 <skmutai6@gmail.com>
2024-05-11 13:35:15 +0300

    commit

--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -1,13 +1,12 @@
 import React from "react";
 import Stock from "./Stock";
 
-function PortfolioContainer() {
+function PortfolioContainer({stock}) {
   return (
     <div>
       <h2>My Portfolio</h2>
-      {
-        //render your portfolio stocks here
-      }
+      {/* render your portfolio stocks here */}
+      <Stock key={stock.id} stock={stock} />
     </div>
   );
 }
--- src/components/StockContainer.js
+++ src/components/StockContainer.js
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Stock from "./Stock";
 
 function StockContainer() {
+  const [ stocks, setStocks ] = useState([])
+  useEffect(() => {
+    fetch("http://localhost:3001/stocks")
+      .then((res) => res.json())
+      .then((data) => {
+        setStocks(data)
+      });
+  }, [setStocks])
+
   return (
     <div>
       <h2>Stocks</h2>
       {/* render stock list here*/}
+      {stocks.map((stock) => (
+        <Stock key={stock.id} stock={stock} />
+      ))}
+
     </div>
   );
 }
